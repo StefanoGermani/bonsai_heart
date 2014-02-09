fbdde5a1e9d5e688709c23ea3ae1f5f2752e5790
Stefano Germani <stefger88@gmail.com>
2014-02-09 17:28:39 +0000

    added HomeController

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 RikkuWeb::Application.routes.draw do
-  root :to => 'assets#index'
+
+  root :to => 'home#index'
+
+  get 'home/index'
   get 'assets/index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
