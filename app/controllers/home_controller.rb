08dfcc9896b73b0cf2d89a9537275a06d2aad6cd
Stefano Germani <stefger88@gmail.com>
2014-02-18 23:14:04 +0000

    added home_controller tests

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
 
-  before_action :open_ember_app_if_user_signed_in
+  before_action :open_ember_app_if_user_already_signed_in
 
   def index
     @user = User.new
@@ -9,7 +9,7 @@ class HomeController < ApplicationController
 
   private
 
-  def open_ember_app_if_user_signed_in
+  def open_ember_app_if_user_already_signed_in
     render 'ember_app/index', layout: false if user_signed_in?
   end
 
--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -2,10 +2,22 @@ require 'spec_helper'
 
 describe HomeController do
 
+
   describe "GET 'index'" do
     subject { get :index }
+    context 'when user is not logged in' do
 
-    it { should be_success }
+      it { should be_success }
+      it { expect(subject).to render_template  :index}
+      it { subject and expect(assigns[:user]).to be_a_new(User) }
+    end
+
+    context 'when user is not logged in' do
+      login_user
+
+      it { expect(subject).to render_template  'ember_app/index'}
+    end
   end
+
 
 end
