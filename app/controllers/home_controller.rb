0aed5a563deb3ab844ecc1ebac38470f17de43e8
Stefano Germani <stefger88@gmail.com>
2014-02-18 21:17:20 +0000

    added signup to homepage and sign in link to header

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,7 @@ class HomeController < ApplicationController
   before_action :open_ember_app_if_user_signed_in
 
   def index
+    @user = User.new
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 BonsaiHeart::Application.routes.draw do
 
-  devise_for :users
+  devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
+
   root :to => 'home#index'
 
   get 'home/index'
